99de3c0b3f62308dbed214574532baff31df26ad
smuhsh <smu_hsh@naver.com>
2019-05-08 22:28:33 +0900

    first commit

---
+++ signal.c
@@ -0,0 +1,22 @@
+/*signal.c*/
+
+#include<stdio.h>
+#include<signal.h>
+#include<unistd.h>
+
+void(*old_fun)(int);
+
+void sigint_handler(int signo){
+
+	printf("ctrl-c 키를 누르셨죠\n");
+//	prinitf("또 누르시면 종료됩니다\n");
+}
+int main(void){
+	old_fun = signal(SIGINT, sigint_handler);
+
+	while(1){
+		printf("signal hander registered!\n");
+		sleep(1);
+	}
+	return (0);
+}
